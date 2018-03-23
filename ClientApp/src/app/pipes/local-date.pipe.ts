913ac2a0dd9342a6a187c7b5dd2441cae55be413
Noelle <noelle.teh@gmail.com>
2018-03-23 09:32:30 -0700

    updated date pipe filter

--- ClientApp/src/app/pipes/local-date.pipe.ts
+++ ClientApp/src/app/pipes/local-date.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LocalDatePipe implements PipeTransform {
 
   transform(value: string, args: string): string {
-    let date = new Date(parseInt(value.substring(0,4)), parseInt(value.substring(5,7))-1, parseInt(value.substring(8,10)), parseInt(value.substring(11,13)), parseInt(value.substring(14,16)));
+    let date = new Date(Date.UTC(parseInt(value.substring(0,4)), parseInt(value.substring(5,7))-1, parseInt(value.substring(8,10)), parseInt(value.substring(11,13)), parseInt(value.substring(14,16))));
     return date.toLocaleString('en-US');
   }
 }
